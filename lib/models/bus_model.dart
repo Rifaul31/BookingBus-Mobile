3ae383bd672d35b7922c3c169e0c66656c53a5e6
Rifaul31 <auliararif@student.telkomuniversity.ac.id>
2022-07-11 04:29:43 +0700

    add connection to DB

--- lib/models/bus_model.dart
+++ lib/models/bus_model.dart
@@ -1,8 +1,8 @@
 class Bus {
   String busCode;
   String busName;
-  String busType;
-  String busSeats;
+  int busSeats;
+  int availableSeats;
   String depLoc;
   String desLoc;
   DateTime depTime;
@@ -12,17 +12,47 @@ class Bus {
   Bus(
     this.busCode,
     this.busName,
-    this.busType,
     this.busSeats,
+    this.availableSeats,
     this.depLoc,
     this.desLoc,
     this.depTime,
     this.arrTime,
     this.price,
   );
+
+  factory Bus.fromJson(Map<String, dynamic> jsonData) {
+    return Bus(
+      jsonData['code'],
+      jsonData['name'],
+      jsonData['seats'],
+      jsonData['available_seats'],
+      jsonData['departure_location'],
+      jsonData['arrive_location'],
+      jsonData['departure_time'],
+      jsonData['arrival_time'],
+      jsonData['price']
+      );
+  }
 }
 
-List<Bus> buses = [
-  Bus('PJ01', 'Primajasa', 'Large', '20', 'Bandung', 'Jakarta', DateTime(2022, 7, 5, 9, 0), DateTime(2022, 7, 5, 10, 0), 100000),
-  Bus('PJ02', 'Primajasa', 'Large', '18', 'Bandung', 'Jakarta', DateTime(2022, 7, 5, 13, 0), DateTime(2022, 7, 5, 14, 0), 100000)
-];
+//List<Bus> buses = [
+//  Bus('PJ01', 'Primajasa', 20, 20, 'Bandung', 'Jakarta', DateTime(2022, 7, 5, 9, 0), DateTime(2022, 7, 5, 10, 0), 100000),
+//  Bus('PJ02', 'Primajasa', 20, 18, 'Bandung', 'Jakarta', DateTime(2022, 7, 5, 13, 0), DateTime(2022, 7, 5, 14, 0), 100000),
+//  Bus('PJ02', 'Metro', 20, 20, 'Palembang', 'Jogjakarta', DateTime(2022, 7, 5, 13, 0), DateTime(2022, 7, 5, 14, 0), 100000),
+//  Bus('PJ01', 'Primajasa', 20, 20, 'Bandung', 'Jakarta', DateTime(2022, 7, 5, 9, 0), DateTime(2022, 7, 5, 10, 0), 100000),
+//  Bus('PJ02', 'Primajasa', 20, 18, 'Bandung', 'Jakarta', DateTime(2022, 7, 5, 13, 0), DateTime(2022, 7, 5, 14, 0), 100000),
+//  Bus('PJ02', 'Metro', 20, 20, 'Palembang', 'Jogjakarta', DateTime(2022, 7, 5, 13, 0), DateTime(2022, 7, 5, 14, 0), 100000),
+//  Bus('PJ01', 'Primajasa', 20, 20, 'Bandung', 'Jakarta', DateTime(2022, 7, 5, 9, 0), DateTime(2022, 7, 5, 10, 0), 100000),
+//  Bus('PJ02', 'Primajasa', 20, 18, 'Bandung', 'Jakarta', DateTime(2022, 7, 5, 13, 0), DateTime(2022, 7, 5, 14, 0), 100000),
+//  Bus('PJ02', 'Metro', 20, 20, 'Palembang', 'Jogjakarta', DateTime(2022, 7, 5, 13, 0), DateTime(2022, 7, 5, 14, 0), 100000),
+//  Bus('PJ01', 'Primajasa', 20, 20, 'Bandung', 'Jakarta', DateTime(2022, 7, 5, 9, 0), DateTime(2022, 7, 5, 10, 0), 100000),
+//  Bus('PJ02', 'Primajasa', 20, 18, 'Bandung', 'Jakarta', DateTime(2022, 7, 5, 13, 0), DateTime(2022, 7, 5, 14, 0), 100000),
+//  Bus('PJ02', 'Metro', 20, 20, 'Palembang', 'Jogjakarta', DateTime(2022, 7, 5, 13, 0), DateTime(2022, 7, 5, 14, 0), 100000),
+//  Bus('PJ01', 'Primajasa', 20, 20, 'Bandung', 'Jakarta', DateTime(2022, 7, 5, 9, 0), DateTime(2022, 7, 5, 10, 0), 100000),
+//  Bus('PJ02', 'Primajasa', 20, 18, 'Bandung', 'Jakarta', DateTime(2022, 7, 5, 13, 0), DateTime(2022, 7, 5, 14, 0), 100000),
+//  Bus('PJ02', 'Metro', 20, 20, 'Palembang', 'Jogjakarta', DateTime(2022, 7, 5, 13, 0), DateTime(2022, 7, 5, 14, 0), 100000),
+//  Bus('PJ01', 'Primajasa', 20, 20, 'Bandung', 'Jakarta', DateTime(2022, 7, 5, 9, 0), DateTime(2022, 7, 5, 10, 0), 100000),
+//  Bus('PJ02', 'Primajasa', 20, 18, 'Bandung', 'Jakarta', DateTime(2022, 7, 5, 13, 0), DateTime(2022, 7, 5, 14, 0), 100000),
+//  Bus('PJ02', 'Metro', 20, 20, 'Palembang', 'Jogjakarta', DateTime(2022, 7, 5, 13, 0), DateTime(2022, 7, 5, 14, 0), 100000),
+//];
--- lib/pages/bus_list_page.dart
+++ lib/pages/bus_list_page.dart
@@ -1,54 +1,81 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:boobus/models/bus_model.dart';
+import 'package:intl/intl.dart';
+import 'package:http/http.dart' as http;
 
 class BusListPage extends StatefulWidget{
+  const BusListPage({Key? key}) : super(key: key);
+
   @override
   _BusListPageState createState() => _BusListPageState();
 }
 
-class _BusListPageState extends State<BusListPage> {
+Future<List<Bus>> downloadJSON() async{
+  final jsonEndPoint = Uri.parse('http://10.0.2.2/boobus/index.php');
 
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: <Widget>[
-          Expanded (
-            child: ListView.builder(
+  final response = await http.get(jsonEndPoint);
+
+  if (response.statusCode == 200) {
+    List buses = json.decode(response.body);
+    return buses.map((bus) => Bus.fromJson(bus)).toList();
+  } else {
+    throw Exception("Unable to download successfully from JSON data");
+  }
+}
+
+class CustomListView extends StatelessWidget {
+  final List<Bus> buses;
+
+  CustomListView(this.buses);
+
+  Widget build(context) {
+    return ListView.builder(
             itemCount: buses.length,
             itemBuilder: (BuildContext context, int index) {
               Bus bus = buses[index];
               return Stack(
                 children: <Widget>[
                   Container(
-                    margin: EdgeInsets.fromLTRB(20, 5, 20, 5),
-                    height: 170,
+                    margin: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                    height: 110,
                     width: double.infinity,
                     decoration: BoxDecoration(
-                      color: Colors.blueAccent,
+                      color: Colors.blue,
                       borderRadius: BorderRadius.circular(15)
                     ),
                     child: Column(
                       children: <Widget>[
+                        const SizedBox(height: 5,),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: <Widget>[
-                            Text(bus.busName, style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),),
-                            Text('\Rp.${bus.price}')
+                            Text('  ${bus.busName}', style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600, color: Colors.white),),
+                            Text('Rp.${bus.price}  ', style: const TextStyle(color: Colors.white))
                           ],
                         ),
+                        const SizedBox(height: 3,),
+                        Row(
+                          children: <Widget>[
+                            Text('  ${bus.depLoc}', style: const TextStyle(color: Colors.white),),
+                            const Text(' - ', style: TextStyle(color: Colors.white)),
+                            Text('${bus.desLoc}  ', style: const TextStyle(color: Colors.white))
+                          ],
+                        ),
+                        const SizedBox(height: 20,),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: <Widget>[
-                            Text(bus.busType),
-                            Text(bus.busSeats)
+                            Text('  ${bus.busSeats} Seats', style: const TextStyle(color: Colors.white),)
                           ],
                         ),
+                        const SizedBox(height: 3,),
                         Row(
                           children: <Widget>[
-                            Text('DepartureTime'),
-                            Text('ArrivalTime')
+                            Text('  ${DateFormat.Hm().format(bus.depTime)}', style: const TextStyle(color: Colors.white)),
+                            const Text(' - ', style: TextStyle(color: Colors.white)),
+                            Text(DateFormat.Hm().format(bus.arrTime), style: const TextStyle(color: Colors.white))
                           ],
                         )
                       ],
@@ -57,7 +84,29 @@ class _BusListPageState extends State<BusListPage> {
                 ],
               );
             }
-            )
+    );
+  }
+}
+
+class _BusListPageState extends State<BusListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Expanded (
+            child: FutureBuilder<List<Bus>>(
+              future: downloadJSON(),
+              builder: (context, snapshot){
+                if (snapshot.hasData) {
+                  List<Bus>? buses = snapshot.data;
+                  return CustomListView(buses!);
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+                return CircularProgressIndicator();
+              }
+            ) 
           )
         ],
       ),
